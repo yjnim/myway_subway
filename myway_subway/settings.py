399491b0ac9644cee67db514668014152ce13761
haeseoklee <interpret96@gmail.com>
2019-03-08 00:47:40 +0900

    a

--- myway_subway/settings.py
+++ myway_subway/settings.py
@@ -149,8 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
