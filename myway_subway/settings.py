98b2b16630495e57c042a14fb42f39118b276a6f
haeseoklee <interpret96@gmail.com>
2019-03-07 23:18:52 +0900

    modify host

--- myway_subway/settings.py
+++ myway_subway/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','ht@sbz2e1i0&*ty+u0!o(3@gtlz6_kn
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
